a2b1670be0b3eeee1dad5356f033abad59ae48d1
Raphael Bandeira <rbandeir@gmail.com>
2020-05-05 19:55:36 -0300

    Adicionar arquivos de projeto.

---
+++ ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ConsultarCEP.Servico.Modelo;
+using ConsultarCEP.Servico;
+using System.Text.RegularExpressions;
+
+namespace ConsultarCEP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            //TODO - lógica
+
+            //TODO - Validações
+            string cep = CEP.Text.Trim();
+
+            if (isValidCEP(cep)) {
+                try
+                {
+                    Endereco end = VIACepServico.BuscaEnderecoViaCEP(cep);
+                    if (end != null)
+                        RESULTADO.Text = string.Format("Endereço: {2} de {3} {0},{1} ", end.localidade, end.uf, end.logradouro, end.bairro);
+                    else
+                    {
+                        DisplayAlert("Erro", "O endereço não foi encontrado para o cep " + cep, "OK");
+                        RESULTADO.Text = "";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("Erro Crítico", ex.Message, "OK");
+                    RESULTADO.Text = "";
+                }
+            }
+            else {
+                DisplayAlert("Erro", "CEP Inválido.", "OK");
+            }
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            cep = cep.Replace(".", "");
+            cep = cep.Replace("-", "");
+            cep = cep.Replace(" ", "");
+
+            Regex Rgx = new Regex(@"^\d{8}$");
+            if (!Rgx.IsMatch(cep))
+                return false;
+            else
+                return true;
+        }
+
+    }
+}
---
+++ ConsultarCEP/Servico/VIACepServico.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace ConsultarCEP.Servico
+{
+    public class VIACepServico
+    {
+
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscaEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc =new WebClient();
+            string conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            if (end.cep == null)
+                return null;
+            else
+                return end;
+        }
+
+    }
+}
